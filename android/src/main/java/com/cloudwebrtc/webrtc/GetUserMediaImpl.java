29d932b84b2b045e17037ae4253c9ac28adad18d
cnwangxiao <67683867+cnwangxiao@users.noreply.github.com>
2022-07-12 12:05:06 +0800

    remove mSurfaceTextureHelpers

--- android/src/main/java/com/cloudwebrtc/webrtc/GetUserMediaImpl.java
+++ android/src/main/java/com/cloudwebrtc/webrtc/GetUserMediaImpl.java
@@ -98,7 +98,6 @@ class GetUserMediaImpl {
     static final String TAG = FlutterWebRTCPlugin.TAG;
 
     private final Map<String, VideoCapturerInfo> mVideoCapturers = new HashMap<>();
-    private final Map<String, SurfaceTextureHelper> mSurfaceTextureHelpers = new HashMap<>();
 
     private final StateProvider stateProvider;
     private final Context applicationContext;
@@ -713,7 +712,6 @@ class GetUserMediaImpl {
 
         String trackId = stateProvider.getNextTrackUUID();
         mVideoCapturers.put(trackId, info);
-        mSurfaceTextureHelpers.put(trackId, surfaceTextureHelper);
 
         Log.d(TAG, "changeCaptureFormat: " + info.width + "x" + info.height + "@" + info.fps);
         videoSource.adaptOutputFormat(info.width, info.height, info.fps);
@@ -729,12 +727,6 @@ class GetUserMediaImpl {
             } catch (InterruptedException e) {
                 Log.e(TAG, "removeVideoCapturer() Failed to stop video capturer");
             } finally {
-
-                SurfaceTextureHelper helper = mSurfaceTextureHelpers.get(id);
-                helper.stopListening();
-                helper.dispose();
-                mSurfaceTextureHelpers.remove(id);
-
                 info.capturer.dispose();
                 mVideoCapturers.remove(id);
             }
