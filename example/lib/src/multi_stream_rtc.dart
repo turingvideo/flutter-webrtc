c18ce06dd98ea14a711b18bbb671f4c496c13a19
yunhe <liyunhe@turingvideo.net>
2023-05-26 11:41:55 +0800

    change

--- example/lib/src/multi_stream_rtc.dart
+++ example/lib/src/multi_stream_rtc.dart
@@ -8,14 +8,12 @@ import 'player/webrtc_player_controls.dart';
 import 'player/webrtc_player_state.dart';
 
 const streamUrls = <String>[
-  'webrtc://test-srs-1.turingvideo.com/live/21315/32724c702e5f87b1d1acaa2529f34a46',
-  'webrtc://test-srs-2.turingvideo.com/live/21314/122e006caf453a6cbd5db4f47f6d4d53',
-  'webrtc://test-srs-2.turingvideo.com/live/21313/6244bebee3abb219a83914d739997d6d',
-  'webrtc://test-srs-1.turingvideo.com/live/21316/0a87fa37d17d3f906af841d16e5d821c',
-  'webrtc://test-srs-1.turingvideo.com/live/21318/f18842edf3576b9c733fc42d63c565c3',
-  'webrtc://test-srs-2.turingvideo.com/live/21325/c6d61b13aea02beff60702a0dfaf2860',
+  'webrtc://test-srs-2.turingvideo.com/live/21328/1b9977cf6fb4516c02188f0208b9efbf',
+  'webrtc://test-srs-2.turingvideo.com/live/21316/0a87fa37d17d3f906af841d16e5d821c',
   'webrtc://test-srs-2.turingvideo.com/live/21327/afba649615e515e3d8105d2b48c41d51',
-  'webrtc://test-srs-1.turingvideo.com/live/21328/1b9977cf6fb4516c02188f0208b9efbf',
+  'webrtc://test-srs-2.turingvideo.com/live/21318/f18842edf3576b9c733fc42d63c565c3',
+  'webrtc://test-srs-2.turingvideo.com/live/21314/122e006caf453a6cbd5db4f47f6d4d53',
+  'webrtc://test-srs-2.turingvideo.com/live/21315/32724c702e5f87b1d1acaa2529f34a46',
 ];
 
 class MultiRTCStream extends StatefulWidget {
@@ -32,8 +30,6 @@ class _MultiRTCStreamState extends State<MultiRTCStream> {
   WebRTCPlayerController playerController4 = WebRTCPlayerController();
   WebRTCPlayerController playerController5 = WebRTCPlayerController();
   WebRTCPlayerController playerController6 = WebRTCPlayerController();
-  WebRTCPlayerController playerController7 = WebRTCPlayerController();
-  WebRTCPlayerController playerController8 = WebRTCPlayerController();
 
   void play() {
     playerController1.setDataSource(url: streamUrls[0]);
@@ -41,9 +37,7 @@ class _MultiRTCStreamState extends State<MultiRTCStream> {
     playerController3.setDataSource(url: streamUrls[2]);
     playerController4.setDataSource(url: streamUrls[3]);
     playerController5.setDataSource(url: streamUrls[4]);
-    // playerController6.setDataSource(url: streamUrls[5]);
-    // playerController7.setDataSource(url: streamUrls[6]);
-    // playerController8.setDataSource(url: streamUrls[7]);
+    playerController6.setDataSource(url: streamUrls[5]);
   }
 
   @override
@@ -54,8 +48,6 @@ class _MultiRTCStreamState extends State<MultiRTCStream> {
     playerController4.dispose();
     playerController5.dispose();
     playerController6.dispose();
-    playerController7.dispose();
-    playerController8.dispose();
 
     super.dispose();
   }
@@ -169,54 +161,22 @@ class _MultiRTCStreamState extends State<MultiRTCStream> {
                 },
               ),
             ),
-            // SizedBox(
-            //   width: w,
-            //   height: h,
-            //   child: WebRTCPlayerControls(
-            //     code: WebrtcCodeType.h264,
-            //     controller: playerController6,
-            //     panelBuilder: (context, rect) {
-            //       return WebrtcPlayerPanel(
-            //         controller: playerController6,
-            //         texturePos: rect,
-            //         settings: PlayerSettings.live(),
-            //         builder: (context) => MultiRTCView(w, h),
-            //       );
-            //     },
-            //   ),
-            // ),
-            // SizedBox(
-            //   width: w,
-            //   height: h,
-            //   child: WebRTCPlayerControls(
-            //     code: WebrtcCodeType.h264,
-            //     controller: playerController7,
-            //     panelBuilder: (context, rect) {
-            //       return WebrtcPlayerPanel(
-            //         controller: playerController7,
-            //         texturePos: rect,
-            //         settings: PlayerSettings.live(),
-            //         builder: (context) => MultiRTCView(w, h),
-            //       );
-            //     },
-            //   ),
-            // ),
-            // SizedBox(
-            //   width: w,
-            //   height: h,
-            //   child: WebRTCPlayerControls(
-            //     code: WebrtcCodeType.h264,
-            //     controller: playerController8,
-            //     panelBuilder: (context, rect) {
-            //       return WebrtcPlayerPanel(
-            //         controller: playerController8,
-            //         texturePos: rect,
-            //         settings: PlayerSettings.live(),
-            //         builder: (context) => MultiRTCView(w, h),
-            //       );
-            //     },
-            //   ),
-            // ),
+            SizedBox(
+              width: w,
+              height: h,
+              child: WebRTCPlayerControls(
+                code: WebrtcCodeType.h264,
+                controller: playerController6,
+                panelBuilder: (context, rect) {
+                  return WebrtcPlayerPanel(
+                    controller: playerController6,
+                    texturePos: rect,
+                    settings: PlayerSettings.live(),
+                    builder: (context) => MultiRTCView(w, h),
+                  );
+                },
+              ),
+            ),
           ],
         ),
         //   ],
